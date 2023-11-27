c3fe5996f11f79fcc0158ca884b046e8d2c18dd5
Diatoz <diatoz@Diatozs-MacBook-Pro.local>
2023-11-27 15:38:02 +0530

    minor file change

--- App.js
+++ App.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import ProductDetails from './src/screens/ProductDetails';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
