30db9c62e6c88338c03ef4c37a472f5ea2943a4a
Diatoz <diatoz@Diatozs-MacBook-Pro.local>
2023-11-27 15:52:25 +0530

    styling changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -60,22 +60,12 @@ const Home = ({navigation}) => {
               {i <= rating ? (
                 <Image
                   source={require('../assets/icons/starFull.png')}
-                  style={{
-                    width: 10,
-                    height: 10,
-                    resizeMode: 'contain',
-                    marginRight: 4,
-                  }}
+                  style={styles.star}
                 />
               ) : (
                 <Image
                   source={require('../assets/icons/star.png')}
-                  style={{
-                    width: 10,
-                    height: 10,
-                    resizeMode: 'contain',
-                    marginRight: 4,
-                  }}
+                  style={styles.star}
                 />
               )}
             </View>,
@@ -83,188 +73,99 @@ const Home = ({navigation}) => {
         }
         return stars;
       };
-
       return (
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <View style={styles.ratingStars}>
           {renderStars()}
-          <Text style={{fontSize: 12}}>{item.rating.rate}</Text>
+          <Text style={styles.rating}>{item.rating.rate}</Text>
         </View>
       );
     };
+
     return (
       <TouchableOpacity
-        style={{
-          backgroundColor: 'white',
-          margin: 10,
-          padding: 10,
-          width: '45%',
-        }}
+        style={styles.product}
         onPress={() =>
           navigation.navigate('ProductDetails', {
             product: item,
             navigation: navigation,
           })
         }>
-        <Image
-          source={{uri: item.image}}
-          style={{
-            width: 160,
-            height: 170,
-            resizeMode: 'contain',
-            marginVertical: 15,
-          }}
-        />
+        <Image source={{uri: item.image}} style={styles.productImage} />
         <StarRating rating={item.rating.rate} />
-        <Text
-          style={{
-            fontSize: 15,
-            fontWeight: 'bold',
-            textTransform: 'capitalize',
-            color: 'black',
-          }}>
-          {item.category}
-        </Text>
-        <Text style={{fontSize: 12}}>{item.title}</Text>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}>
+        <Text style={styles.productCategory}>{item.category}</Text>
+        <Text style={styles.rating}>{item.title}</Text>
+        <View style={styles.ratingStars}>
           <Image
             source={require('../assets/icons/rupee.png')}
-            style={{
-              width: 10,
-              height: 10,
-            }}
+            style={styles.rupee}
           />
-          <Text style={{fontSize: 14}}>{item.price}</Text>
+          <Text style={styles.price}>{item.price}</Text>
         </View>
       </TouchableOpacity>
     );
   };
 
   return (
-    <View style={{flex: 1, marginBottom: 100}}>
+    <View style={styles.mainContainer}>
       {/* HEADER */}
-      <View
-        style={{
-          flexDirection: 'row',
-          backgroundColor: 'white',
-          padding: 10,
-          height: '7%',
-          alignItems: 'center',
-        }}>
-        <View
-          style={{
-            width: '70%',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-          }}>
+      <View style={styles.header}>
+        <View style={styles.leftMenu}>
           <Image
             source={require('../assets/icons/menu.png')}
-            style={{
-              width: 20,
-              height: 20,
-              resizeMode: 'contain',
-              marginRight: 15,
-            }}
+            style={styles.menu}
           />
           {onSearch ? (
             <TextInput
-              style={{
-                borderWidth: 1,
-                borderColor: 'lightgray',
-                padding: 5,
-                width: '89%',
-              }}
+              style={styles.searchBox}
               placeholder="Search..."
               onChangeText={text => setSearch(text)}
               value={search}
             />
           ) : (
-            <Text style={{fontSize: 17}}>Women</Text>
+            <Text style={styles.categoryTitle}>Women</Text>
           )}
         </View>
-        <View
-          style={{
-            width: '30%',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-          }}>
+        <View style={styles.rightMenu}>
           <TouchableOpacity onPress={() => setOnSearch(true)}>
             <Image
               source={require('../assets/icons/search.png')}
-              style={{
-                width: 20,
-                height: 20,
-                resizeMode: 'contain',
-                marginRight: 15,
-              }}
+              style={styles.search}
             />
           </TouchableOpacity>
           <Image
             source={require('../assets/icons/heart.png')}
-            style={{
-              width: 20,
-              height: 20,
-              resizeMode: 'contain',
-              marginRight: 15,
-            }}
+            style={styles.search}
           />
           <Image
             source={require('../assets/icons/cart.png')}
-            style={{width: 20, height: 20, resizeMode: 'contain'}}
+            style={styles.cart}
           />
         </View>
       </View>
 
       {/* NAVBAR */}
-      <View
-        style={{
-          flexDirection: 'row',
-          margin: 14,
-          alignItems: 'center',
-        }}>
-        <Text style={{fontSize: 13, alignSelf: 'flex-start', width: '70%'}}>
-          {totalItemCount} items
-        </Text>
-        <View
-          style={{
-            width: '30%',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-          }}>
+      <View style={styles.navbar}>
+        <Text style={styles.itemCount}>{totalItemCount} items</Text>
+        <View style={styles.itemViewFormat}>
           <TouchableOpacity onPress={() => setIsGridView(!isGridView)}>
             <Image
               source={require('../assets/icons/list.png')}
-              style={{
-                width: 16,
-                height: 16,
-                resizeMode: 'contain',
-                marginRight: 10,
-              }}
-              tintColor={!isGridView ? 'gray' : 'lightgray'}
+              style={styles.listView}
+              tintColor={!isGridView ? 'black' : 'gray'}
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => setIsGridView(!isGridView)}>
             <Image
               source={require('../assets/icons/grid.png')}
-              style={{
-                width: 16,
-                height: 16,
-                resizeMode: 'contain',
-              }}
-              tintColor={isGridView ? 'gray' : 'lightgray'}
+              style={styles.grid}
+              tintColor={isGridView ? 'black' : 'gray'}
             />
           </TouchableOpacity>
         </View>
       </View>
 
       {/* ITEM LIST */}
-      <View style={{marginHorizontal: 10}}>
+      <View style={styles.list}>
         <FlatList
           data={onSearch ? filteredData : products}
           renderItem={renderItem}
@@ -272,6 +173,22 @@ const Home = ({navigation}) => {
           numColumns={isGridView ? 2 : 1}
           keyExtractor={item => item.id}
         />
+        {/* logic for date group */}
+        {/* <FlatList
+          data={Object.entries(groupedData)}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <View>
+              <Text style={styles.date}>{item[0]}</Text>
+              <FlatList
+                data={onSearch ? filteredData : products}
+                key={isGridView ? 'grid' : 'list'}
+                numColumns={isGridView ? 2 : 1}
+                renderItem={renderItem}
+              />
+            </View>
+          )}
+        /> */}
       </View>
     </View>
   );
@@ -281,13 +198,99 @@ export default Home;
 
 const styles = StyleSheet.create({
   star: {
-    fontSize: 20,
-    color: 'gray',
-    marginRight: 5,
+    width: 10,
+    height: 10,
+    resizeMode: 'contain',
+    marginRight: 4,
   },
-  starFilled: {
-    fontSize: 20,
-    color: 'gold',
-    marginRight: 5,
+  ratingStars: {flexDirection: 'row', alignItems: 'center'},
+  rating: {fontSize: 12},
+  product: {
+    backgroundColor: 'white',
+    margin: 10,
+    padding: 10,
+    width: '45%',
   },
+  productImage: {
+    width: 160,
+    height: 170,
+    resizeMode: 'contain',
+    marginVertical: 15,
+  },
+  productCategory: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    textTransform: 'capitalize',
+    color: 'black',
+  },
+  rupee: {
+    width: 10,
+    height: 10,
+  },
+  price: {fontSize: 14},
+  mainContainer: {flex: 1, marginBottom: 100},
+  header: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10,
+    height: '7%',
+    alignItems: 'center',
+  },
+  leftMenu: {
+    width: '70%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  menu: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    marginRight: 15,
+  },
+  searchBox: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    padding: 5,
+    width: '89%',
+  },
+  categoryTitle: {fontSize: 17},
+  rightMenu: {
+    width: '30%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  search: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    marginRight: 15,
+  },
+  cart: {width: 20, height: 20, resizeMode: 'contain'},
+  navbar: {
+    flexDirection: 'row',
+    margin: 14,
+    alignItems: 'center',
+  },
+  itemCount: {fontSize: 13, alignSelf: 'flex-start', width: '70%'},
+  itemViewFormat: {
+    width: '30%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  listView: {
+    width: 16,
+    height: 16,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+  grid: {
+    width: 16,
+    height: 16,
+    resizeMode: 'contain',
+  },
+  list: {marginHorizontal: 10},
+  // date:{fontSize: 18, fontWeight: 'bold'}
 });
--- src/screens/ProductDetails.js
+++ src/screens/ProductDetails.js
@@ -13,175 +13,165 @@ const ProductDetails = ({route}) => {
   const {product, navigation} = route.params;
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <ScrollView
-        contentContainerStyle={{flexGrow: 1, margin: 20}}
-        scrollEnabled={true}>
-        {/* HEADER */}
-        <View
-          style={{
-            flexDirection: 'row',
-            height: '7%',
-            alignItems: 'center',
-          }}>
-          <View
-            style={{
-              width: '70%',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'flex-start',
-            }}>
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-              <Image
-                source={require('../assets/icons/arrowBack.png')}
-                style={{
-                  width: 20,
-                  height: 20,
-                }}
-              />
-            </TouchableOpacity>
+    <ScrollView
+      contentContainerStyle={styles.mainContainer}
+      scrollEnabled={true}>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <View style={styles.leftMenu}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Image
+              source={require('../assets/icons/arrowBack.png')}
+              style={styles.back}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.rightMenu}>
+          <View style={styles.wishlist}>
+            <Image
+              source={require('../assets/icons/heart.png')}
+              style={styles.back}
+            />
           </View>
-          <View
-            style={{
-              width: '30%',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-            }}>
-            <View
-              style={{
-                height: 35,
-                width: 35,
-                backgroundColor: 'white',
-                borderRadius: 50,
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginRight: 10,
-              }}>
-              <Image
-                source={require('../assets/icons/heart.png')}
-                style={{
-                  width: 20,
-                  height: 20,
-                }}
-              />
-            </View>
-            <View
-              style={{
-                height: 35,
-                width: 35,
-                backgroundColor: 'white',
-                borderRadius: 50,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                source={require('../assets/icons/cart.png')}
-                style={{width: 20, height: 20}}
-              />
-            </View>
+          <View style={styles.cart}>
+            <Image
+              source={require('../assets/icons/cart.png')}
+              style={styles.back}
+            />
           </View>
+        </View>
+      </View>
+
+      {/* product detail */}
+      <View>
+        {/* image */}
+        <View style={styles.imageContainer}>
+          <Image source={{uri: product.image}} style={styles.image} />
         </View>
 
         {/* product detail */}
-        <View>
-          {/* image */}
-          <View
-            style={{
-              borderRadius: 10,
-              marginBottom: 30,
-              backgroundColor: 'white',
-              justifyContent: 'center',
-              alignItems: 'center',
-              elevation: 3,
-              shadowRadius: 10,
-              width: '100%',
-              height: '50%',
-            }}>
-            <Image
-              source={{uri: product.image}}
-              style={{
-                width: '80%',
-                height: '80%',
-                resizeMode: 'center',
-              }}
-            />
-          </View>
-
-          {/* product detail */}
-          <View style={{marginBottom: 20}}>
-            <Text
-              style={{
-                fontSize: 15,
-                textTransform: 'capitalize',
-              }}>
-              {product.category}
-            </Text>
-            <Text
-              style={{
-                fontSize: 20,
-                fontWeight: 'bold',
-                color: 'black',
-                marginBottom: 20,
-              }}>
-              {product.title}
-            </Text>
-            <Text style={{fontSize: 14}}>Description</Text>
-            <Text style={{fontSize: 14, color: 'black'}}>
-              {product.description}
-            </Text>
-          </View>
-
-          {/* rating */}
-          <View style={{marginBottom: 10}}>
-            <Text style={{fontSize: 14, marginBottom: 5}}>Ratings</Text>
-            <Text style={{fontSize: 12}}>{product.rating.rate}</Text>
-          </View>
-
-          {/* divider */}
-          <View
-            style={{
-              borderBottomWidth: 1,
-              borderBottomColor: 'lightgray',
-              marginBottom: 10,
-            }}
-          />
-
-          {/* cart */}
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}>
-            <View>
-              <Text style={{fontSize: 13}}>Price</Text>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <Text
-                  style={{fontSize: 16, fontWeight: 'bold', color: 'black'}}>
-                  ${product.price}
-                </Text>
-              </View>
-            </View>
-            <Text
-              style={{
-                fontSize: 16,
-                color: 'white',
-                fontWeight: 'bold',
-                backgroundColor: '#ef5201',
-                borderRadius: 10,
-                paddingVertical: 10,
-                paddingHorizontal: 35,
-              }}>
-              Add to Cart
-            </Text>
-          </View>
+        <View style={styles.detail}>
+          <Text style={styles.category}>{product.category}</Text>
+          <Text style={styles.title}>{product.title}</Text>
+          <Text style={styles.description}>Description</Text>
+          <Text style={styles.descriptionContent}>{product.description}</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+        {/* rating */}
+        <View style={styles.rating}>
+          <Text style={styles.ratings}>Ratings</Text>
+          <Text style={styles.ratingNumber}>{product.rating.rate}</Text>
+        </View>
+
+        {/* divider */}
+        <View style={styles.divider} />
+
+        {/* cart */}
+        <View style={styles.bottomContainer}>
+          <View>
+            <Text style={styles.price}>Price</Text>
+            <Text style={styles.priceValue}>${product.price}</Text>
+          </View>
+          <Text style={styles.btn}>Add to Cart</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 export default ProductDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {flexGrow: 1, margin: 20},
+  header: {
+    flexDirection: 'row',
+    height: '7%',
+    alignItems: 'center',
+  },
+  leftMenu: {
+    width: '70%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  back: {
+    width: 20,
+    height: 20,
+  },
+  rightMenu: {
+    width: '30%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  wishlist: {
+    height: 35,
+    width: 35,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  cart: {
+    height: 35,
+    width: 35,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    borderRadius: 10,
+    marginBottom: 30,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 3,
+    shadowRadius: 10,
+    width: '100%',
+    height: '50%',
+  },
+  image: {
+    width: '80%',
+    height: '80%',
+    resizeMode: 'center',
+  },
+  detail: {marginBottom: 20},
+  category: {
+    fontSize: 15,
+    textTransform: 'capitalize',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 20,
+  },
+  description: {fontSize: 14},
+  descriptionContent: {fontSize: 14, color: 'black'},
+  rating: {marginBottom: 10},
+  ratings: {fontSize: 14, marginBottom: 5},
+  ratingNumber: {fontSize: 12},
+  divider: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray',
+    marginBottom: 10,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  price: {fontSize: 13},
+  priceValue: {fontSize: 16, fontWeight: 'bold', color: 'black'},
+  btn: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+    backgroundColor: '#ef5201',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 35,
+  },
+});
